a3901bb7a38df4695c4d07367d20559d056df653
ThePlayerRolo <thesolis78243@gmail.com>
2026-05-15 20:59:44 -0400

    GXFifo

--- configure.py
+++ configure.py
@@ -412,6 +412,7 @@ config.libs = [
 
     DolphinLib("gx", [
         Object(Matching, "gx/GXInit.c"),
+        Object(Matching, "gx/GXFifo.c"),
     ]),
     MSL_C([
         Object(Matching, "MSL/MSL_C/MSL_Common/src/string.c"),
