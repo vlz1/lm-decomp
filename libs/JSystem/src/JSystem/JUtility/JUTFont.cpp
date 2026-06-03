adca7e215d67b2f655739288c555f43e013ca0b8
ThePlayerRolo <thesolis78243@gmail.com>
2026-06-03 15:58:49 -0400

    remove draw_string_size_scale that is a 0

--- libs/JSystem/src/JSystem/JUtility/JUTFont.cpp
+++ libs/JSystem/src/JSystem/JUtility/JUTFont.cpp
@@ -28,24 +28,3 @@ void JUTFont::setGradColor(JUtility::TColor col1, JUtility::TColor col2)
 	mColor3 = col2;
 	mColor4 = col2;
 }
-
-f32 JUTFont::drawString_size_scale(f32 a1, f32 a2, f32 a3, f32 a4,
-                                   const char* str, u32 usz, bool a7)
-{
-	f32 temp = a1;
-
-	for (; usz > 0; usz--, str++) {
-		u32 b = *(u8*)str;
-		/*if (isLeadByte(b)) {
-			str++;
-			b <<= 8;
-			b |= *(u8*)str;
-			usz--;
-		}*/
-
-		a1 += drawChar_scale(a1, a2, a3, a4, b, a7);
-		a7 = 1;
-	}
-
-	return a1 - temp;
-}
