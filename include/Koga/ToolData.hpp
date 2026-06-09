d4e16f8ee5aa561549712e119aeeb5b886a45928
SomeJakeGuy <39894486+SomeJakeGuy@users.noreply.github.com>
2026-06-09 08:53:57 -0400

    Matched missing % of ToolData, including introducing some inline functions that seem to get used in other classes. Extended split to include a few more functions. (#10)
    * Matched missing % of ToolData, including introducing some inline functions that seem to get used in other classes. Extended functions to include a few more.
    
    * Missed a const declaration

--- include/Koga/ToolData.hpp
+++ include/Koga/ToolData.hpp
@@ -13,14 +13,14 @@ namespace Koga {
         ToolData();
         virtual ~ToolData();
 
-    #define JMAP_VALUE_TYPE_LONG       0
-    #define JMAP_VALUE_TYPE_STRING     1
-    #define JMAP_VALUE_TYPE_FLOAT      2
-    #define JMAP_VALUE_TYPE_LONG_2     3
-    #define JMAP_VALUE_TYPE_SHORT      4
-    #define JMAP_VALUE_TYPE_BYTE       5
-    #define JMAP_VALUE_TYPE_STRING_PTR 6
-    #define JMAP_VALUE_TYPE_NULL       7
+        #define JMAP_VALUE_TYPE_LONG       0
+        #define JMAP_VALUE_TYPE_STRING     1
+        #define JMAP_VALUE_TYPE_FLOAT      2
+        #define JMAP_VALUE_TYPE_LONG_2     3
+        #define JMAP_VALUE_TYPE_SHORT      4
+        #define JMAP_VALUE_TYPE_BYTE       5
+        #define JMAP_VALUE_TYPE_STRING_PTR 6
+        #define JMAP_VALUE_TYPE_NULL       7
 
         struct JMapItem {
             u32 mHash;     // 0x0
@@ -42,15 +42,42 @@ namespace Koga {
             const JMapItem mItems[]; // 0x10
         };
 
-        BOOL attach(const JMapData* data);
+        inline const JMapData* getJMapData() {
+            return mData;
+        }
+
+        inline void setJMapData(JMapData* pData) {
+            mData = pData;
+        }
+
+        template<typename T>
+        inline T getUnsignedValue(int entryIndex, int fieldIndex) const {
+            const JMapItem* pField = &mData->mItems[fieldIndex];
+            const void* ptr = reinterpret_cast<const u32*>(reinterpret_cast<const u8*>(mData) + mData->mDataOffset + 
+                (entryIndex * mData->mEntrySize) + pField->mOffsData);
+            return (*reinterpret_cast<const T*>(ptr) & pField->mMask) >> pField->mShift;
+        }
+
+        template<typename T>
+        inline const T getSignedValue(int entryIndex, int fieldIndex) const {
+            const void* ptr = getDataPointer(mData, &mData->mItems[fieldIndex], entryIndex, fieldIndex);
+            return *reinterpret_cast<const T*>(ptr);
+        }
+
+        BOOL attach(const JMapData*);
         void reset();
-        s32 searchItemInfo(const char* key) const;
-        BOOL getValueMasked(int entryIndex, const char* key, u32* valueOut) const;
-        BOOL getValueBit(int entryIndex, const char* key, u8* bitOut) const;
-        BOOL getValue(int entryIndex, const char* key, s32* valueOut) const;
-        BOOL getValue(int entryIndex, const char* key, f32* valueOut) const;
-        BOOL getValue(int entryIndex, const char* key, const char** valueOut) const;
+        BOOL getValue(int, const char*, u32*) const;
+        BOOL getValue(int, const char*, s32*) const;
+        BOOL getValue(int, const char*, u8*) const;
+        BOOL getValue(int, const char*, f32*) const;
+        BOOL getValue(int, const char*, const char**) const;
+        s32 searchItemInfo(const char*) const;
+        s32 findEntryByValue(int, const u32, int);
+        s32 findEntryByValue(int, const char*, int);
+
     private:
+        /* 0x04 */ const JMapData* mData;
+
         inline u32 hashString(const char* key) const
         {
             u32 stringHash = 0;
@@ -63,7 +90,32 @@ namespace Koga {
             return stringHash;
         }
 
-        /* 0x04 */ const JMapData* mData;
+        inline const void* getDataPointer(const JMapData* pData, const JMapItem* pField, int entryIndex, int fieldIndex) const {
+            return reinterpret_cast<const u32*>(reinterpret_cast<const u8*>(pData) + pData->mDataOffset + 
+                (entryIndex * pData->mEntrySize) + pField->mOffsData);
+        }
+
+        inline const u32 getExpectedUnsignedValue(int entryIndex, int fieldIndex) {
+            const JMapItem* pField = mData->getItem(fieldIndex);
+            const void* ptr = getDataPointer(mData, pField, entryIndex, fieldIndex);
+            return (*reinterpret_cast<const u32*>(ptr) & pField->mMask) >> pField->mShift;
+        }
+
+        inline const f32 getFloatValue(int entryIndex, int fieldIndex) const {
+            return *reinterpret_cast<const f32*>(getDataPointer(mData, &mData->mItems[fieldIndex], entryIndex, fieldIndex));
+        }
+
+        inline const bool getBoolValue(int entryIndex, int fieldIndex) const {
+            const JMapItem* pField = &mData->mItems[fieldIndex];
+            u32 entryOffset = entryIndex * mData->mEntrySize;
+            const void* ptr = reinterpret_cast<const u32*>(reinterpret_cast<const u8*>(mData) + mData->mDataOffset + 
+                entryOffset + pField->mOffsData);
+            return *reinterpret_cast<const u32*>(ptr) & pField->mMask;
+        }
+
+        inline const char* getStringValue(int entryIndex, int fieldIndex) const {
+            return reinterpret_cast<const char*>(getDataPointer(mData, &mData->mItems[fieldIndex], entryIndex, fieldIndex));
+        };
     };
 }
 
--- src/Koga/ToolData.cpp
+++ src/Koga/ToolData.cpp
@@ -18,15 +18,13 @@ void Koga::ToolData::reset() {
 	mData = nullptr;
 }
 
-BOOL Koga::ToolData::getValueMasked(int entryIndex, const char* key, u32* valueOut) const {
+BOOL Koga::ToolData::getValue(int entryIndex, const char* key, u32* valueOut) const {
     s32 itemIndex = searchItemInfo(key);
 	if (itemIndex < 0) {
 		return FALSE;
 	}
 
-    const JMapItem* item = mData->getItem(itemIndex);
-	u32 value = *reinterpret_cast<const u32*>(reinterpret_cast<const u8*>(mData) + mData->mDataOffset + entryIndex * mData->mEntrySize + item->mOffsData);
-	*valueOut = (value & item->mMask) >> item->mShift;
+    *valueOut = getUnsignedValue<u32>(entryIndex, itemIndex);
 	return TRUE;
 }
 
@@ -36,25 +34,17 @@ BOOL Koga::ToolData::getValue(int entryIndex, const char* key, s32* valueOut) co
 		return FALSE;
 	}
 
-	u32 entryOffset = entryIndex * mData->mEntrySize;
-	s32 dataOffset = mData->mDataOffset;
-    const JMapItem* item = &mData->mItems[itemIndex];
-	const u8* valuePtr = reinterpret_cast<const u8*>(mData) + dataOffset + entryOffset + item->mOffsData;
-	s32 value = *reinterpret_cast<const s32*>(valuePtr);
-	*valueOut = value;
+	*valueOut = getSignedValue<s32>(entryIndex, itemIndex);
 	return TRUE;
 }
 
-// https://decomp.me/scratch/yhDD2
-BOOL Koga::ToolData::getValueBit(int entryIndex, const char* key, u8* bitOut) const {
+BOOL Koga::ToolData::getValue(int entryIndex, const char* key, u8* valueOut) const {
 	s32 itemIndex = searchItemInfo(key);
 	if (itemIndex < 0) {
 		return FALSE;
 	}
 
-    const JMapItem* item = mData->getItem(itemIndex);
-	u32 value = *reinterpret_cast<const u32*>(reinterpret_cast<const u8*>(mData) + mData->mDataOffset + entryIndex * mData->mEntrySize + item->mOffsData);
-	*bitOut = ((value & item->mMask) != 0);
+    *valueOut = getBoolValue(entryIndex, itemIndex);
 	return TRUE;
 }
 
@@ -64,11 +54,7 @@ BOOL Koga::ToolData::getValue(int entryIndex, const char* key, const char** valu
 		return FALSE;
 	}
 
-	u32 entryOffset = entryIndex * mData->mEntrySize;
-	s32 dataOffset = mData->mDataOffset;
-    const JMapItem* item = &mData->mItems[itemIndex];
-	const u8* valuePtr = reinterpret_cast<const u8*>(mData) + dataOffset + entryOffset + item->mOffsData;
-	*valueOut = reinterpret_cast<const char*>(valuePtr);
+	*valueOut = getStringValue(entryIndex, itemIndex);
 	return TRUE;
 }
 
@@ -78,11 +64,7 @@ BOOL Koga::ToolData::getValue(int entryIndex, const char* key, f32* valueOut) co
 		return FALSE;
 	}
 
-    u32 entryOffset = entryIndex * mData->mEntrySize;
-	s32 dataOffset = mData->mDataOffset;
-    const JMapItem* item = &mData->mItems[itemIndex];
-	const u8* valuePtr = reinterpret_cast<const u8*>(mData) + dataOffset + entryOffset + item->mOffsData;
-	*valueOut = *reinterpret_cast<const f32*>(valuePtr);
+	*valueOut = getFloatValue(entryIndex, itemIndex);
 	return TRUE;
 }
 
@@ -95,6 +77,35 @@ s32 Koga::ToolData::searchItemInfo(const char* key) const {
 		if (hash == mData->mItems[i].mHash) {
 			return i;
 		}
+	}
+
+	return -1;
+}
+
+s32 Koga::ToolData::findEntryByValue(int fieldIndex, const u32 expectedValue, int entryIndex) {
+	int currIndex = entryIndex;
+	int maxIndex = mData->mNumEntries;
+
+	while (currIndex < maxIndex) {
+		u32 currVal = getExpectedUnsignedValue(currIndex, fieldIndex);
+
+		if (currVal == expectedValue) {
+			return currIndex;
+		}
+
+		currIndex++;
+	}
+
+	return -1;
+}
+
+s32 Koga::ToolData::findEntryByValue(int fieldIndex, const char* cmpStr, int entryIndex) {
+	while (entryIndex < mData->mNumEntries) {
+		const char* currStr = getStringValue(entryIndex, fieldIndex);
+		if (strcmp(currStr, cmpStr) == 0) {
+			return entryIndex;
+		}
+		entryIndex++;
 	}
 
 	return -1;
