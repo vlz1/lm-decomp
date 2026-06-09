d46322e43a8f9d5cbaa28e34544f6b81863796c1
vlez <122755709+vlz1@users.noreply.github.com>
2026-06-09 14:45:36 -0500

    Update JmpMessageSender and fix build errors

--- include/Koga/JMPMessage.hpp
+++ include/Koga/JMPMessage.hpp
@@ -11,10 +11,10 @@ public:
     /* 0x08 */ virtual ~JmpMessageSender();
     /* 0x0C */ virtual void vt_0C();
     /* 0x10 */ virtual void vt_10();
-    /* 0x14 */ virtual s32 vt_14(MessageSenderCallback* param_1, int param_2);
-    /* 0x18 */ virtual void vt_18();
+    /* 0x14 */ virtual s32 vt_14(MessageCallback1 fn, int arg0);
+    /* 0x18 */ virtual s32 vt_18(MessageCallback2 fn, int arg0, int arg1);
 
-    bool fn_800EA684(MessageSenderCallback*);
+    bool fn_800EA684(MessageSender*);
 
 public:
     /* 0x38 */ u8 _38[0xDF0 - sizeof(MessageSender)];
