c6c72382b94da9a56e532664cdd3a2b7fabc5927
ThePlayerRolo <thesolis78243@gmail.com>
2026-06-09 11:54:09 -0400

    rough message progress

--- include/Koga/Message.hpp
+++ include/Koga/Message.hpp
@@ -5,11 +5,51 @@
 
 class MessageSender {
 public:
+    typedef bool (MessageSender::*MessageSenderCallback)(int val);
+
+    class unkSubClass {
+    public:
+        unkSubClass();
+        ~unkSubClass();
+
+        void addSenderCallback(MessageSenderCallback**);
+        s32 getCurrentFuncCount() const { return mCurFuncCount; }
+        MessageSenderCallback getFuncCallback(s32 idx) const { return *mFuncCallbacks[idx]; }
+
+        MessageSenderCallback* mFuncCallbacks[12];
+        s32 mCurFuncCount;
+    };
+
+    MessageSender();
+
     virtual ~MessageSender();
     virtual void vt_0C() = 0;
     virtual void vt_10() = 0;
-    virtual void vt_14();
+    virtual s32 vt_14(MessageSenderCallback* param_1, int param_2);
     virtual void vt_18();
+
+    bool fn_800EA684(MessageSenderCallback*);
+
+public:
+    /* 0x4 */ unkSubClass _4;
+};
+
+class IMessageReciever {
+public:
+    inline virtual ~IMessageReciever() { }
+    virtual s32 vt_0C() = 0;
+    virtual s32 vt_10() = 0;
+    virtual s32 vt_14() = 0;
+};
+
+class MessageReciever : public IMessageReciever {
+public:
+    MessageReciever();
+
+    virtual ~MessageReciever();
+    virtual s32 vt_0C();
+    virtual s32 vt_10();
+    virtual s32 vt_14();
 };
 
 #endif
--- src/Koga/Message.cpp
+++ src/Koga/Message.cpp
@@ -2,6 +2,51 @@
 
 dummy_float_data()
 
-MessageSender::~MessageSender() {
 
+
+MessageReciever::MessageReciever() {}
+
+MessageReciever::~MessageReciever() {}
+
+s32 MessageReciever::vt_0C() { return 0; }
+
+s32 MessageReciever::vt_10() { return 0; }
+
+s32 MessageReciever::vt_14() { return 0; }
+
+MessageSender::MessageSender() { }
+
+MessageSender::~MessageSender() { }
+
+bool MessageSender::fn_800EA684(MessageSenderCallback* param_1) {
+    if (_4.getCurrentFuncCount() < 12) {
+        _4.addSenderCallback(&param_1);
+        return true;
+    }
+
+    return false;
+}
+
+s32 MessageSender::vt_14(MessageSenderCallback* param_1, int param_2) {
+    s32 funcCount = _4.getCurrentFuncCount();
+    MessageSenderCallback* func = _4.mFuncCallbacks[0];
+
+    for (s32 i = 0; i < funcCount; i++) {
+         if ((this->**func)(param_2)) return true;
+        //if (_4.getFuncCallback(i)) return true;
+        //func = ((func) + 1;
+        func = func + 1;
+    }
+    return false;
+}
+
+MessageSender::unkSubClass::unkSubClass()
+{
+    mCurFuncCount = 0;
+}
+
+MessageSender::unkSubClass::~unkSubClass() {}
+
+void MessageSender::unkSubClass::addSenderCallback(MessageSenderCallback** newFunc) {
+    mFuncCallbacks[mCurFuncCount++] = *newFunc;
 }
