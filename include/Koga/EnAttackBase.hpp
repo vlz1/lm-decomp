4ca0f49393fcdfb66faf9c75934c1b31bf17a272
ThePlayerRolo <thesolis78243@gmail.com>
2026-06-12 11:45:37 -0400

    fix EnAttackBase vtable

--- include/Koga/EnAttackBase.hpp
+++ include/Koga/EnAttackBase.hpp
@@ -6,8 +6,13 @@
 
 class EnAttackBase : public EnemyStrategy, public Koga::CharacterEventObserver {
 public:
-    /* 0x08 */ virtual ~EnAttackBase();
-    /* 0x70 */ virtual void* vt_70(); // Seems to be in a different TU than the destructor
+    /* 0x08 */ virtual ~EnAttackBase(); //TODO: Could be weak symbol as well
+    /* 0x70 */ virtual void* vt_70() { return _20; } //Weak symbol
+
+public:
+    /* 0x18 */ s32 _18;
+    /* 0x1C */ s32 _1C;
+    /* 0x20 */ void* _20;
 };
 
 #endif
